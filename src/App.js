c95b13bef092d4a88fe0d11ad942771d18f1eaed
Fateme Narangifard <fatemenarangifard@gmail.com>
2023-11-13 12:07:59 +0330

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import Header from './Components/Layout/Header';
+import Menu from './Components/Layout/Menu';
+import Footer from './Components/Layout/Footer'
+
+import Home from './Pages/Home'
+
+
 const App = () => {
     return (
         <div>
-            <h2>hi</h2>
+           <Header/>
+           <Menu/>
+           <Home/>
+           <Footer/>
         </div>
     );
 };
