6e1b6c6ec191c8f421a371c46ab5ce6f01e02af0
lucifer7ngf <lucifer.ngf@gmail.com>
2023-11-14 11:22:41 +0330

    .

--- src/Components/Layout/Header/index.js
+++ src/Components/Layout/Header/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
+import Logo from "../../../Images/Logo.png";
+import "./Header.css";
+
+
 const index = () => {
+
+
     return (
-        <div>
-            Header
+        <div className='d-flex justify-content-center'>
+            <div className=''>
+                <h3>
+                    <a href="#">
+                    Submite Project
+                    </a>
+                </h3>
+                <h3>
+                    <a href="#">
+                    Advertize
+                    </a>
+                </h3>
+            </div>
+            <div>
+                <div className='img-box'>
+                    <img src={Logo} alt="logo" />    
+                </div>
+            </div>
+            <div className='d-flex justify-content-center'>
+                <button className='me-3'> 
+                    Login
+                </button>
+                <button className='signUp-btn'>
+                    Sign Up
+                </button>
+
+            </div>
         </div>
     );
 };
